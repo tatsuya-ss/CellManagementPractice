ea1a3a3f826c016ff30ce06dcf9c54c215ccdd60
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-12 21:19:57 +0900

    プロジェクト作成

---
+++ CellManagementPractice/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CellManagementPractice
+//
+//  Created by 坂本龍哉 on 2021/10/12.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
